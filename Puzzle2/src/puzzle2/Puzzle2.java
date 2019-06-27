a9c8047b7ca4c5c424798ba744540f5708984137
akashkumawat895 <akashkumawat895@gmail.com>
2019-06-27 12:17:00 +0530

    puzzle game uploaded

---
+++ Puzzle2/src/puzzle2/Puzzle2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package puzzle2;
+
+/**
+ *
+ * @author udeshay
+ */
+public class Puzzle2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+   static Welcome WC = new Welcome();
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WC.setVisible(true);
+        
+    }
+    
+}
